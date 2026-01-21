3e05817d8ee6a86afb8c16fad617937b6aefa8cf
Nasssdev <aissaoui_nassim@outlook.fr>
2026-01-21 21:08:56 +0100

    feat(admin): add admin table repository port

---
+++ back/src/modules/admin/application/ports/admin-table.repository.port.ts
@@ -0,0 +1,14 @@
+import { Table } from '../../../ordering/domain/entities/table.entity';
+
+export const ADMIN_TABLE_REPOSITORY = Symbol('ADMIN_TABLE_REPOSITORY');
+
+export interface IAdminTableRepository {
+  findByRestaurantId(restaurantId: number): Promise<Table[]>;
+  findById(id: number): Promise<Table | null>;
+  create(table: Omit<Table, 'id'>): Promise<Table>;
+  update(
+    id: number,
+    table: Partial<Omit<Table, 'id'>>,
+  ): Promise<Table | null>;
+  delete(id: number): Promise<boolean>;
+}
