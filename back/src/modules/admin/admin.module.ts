4bf0306e8f07f83f4fd875c89fe4cd8702a8db21
Nasssdev <aissaoui_nassim@outlook.fr>
2026-01-21 21:12:39 +0100

    feat(admin): wire up table CRUD in admin module

--- back/src/modules/admin/admin.module.ts
+++ back/src/modules/admin/admin.module.ts
@@ -10,9 +10,11 @@ import { GuestOrmEntity } from '../ordering/infrastructure/persistence/orm-entit
 
 // Ports
 import { ADMIN_RESTAURANT_REPOSITORY } from './application/ports/admin-restaurant.repository.port';
+import { ADMIN_TABLE_REPOSITORY } from './application/ports/admin-table.repository.port';
 
 // Repositories
 import { AdminRestaurantRepository } from './infrastructure/persistence/repositories/admin-restaurant.repository';
+import { AdminTableRepository } from './infrastructure/persistence/repositories/admin-table.repository';
 
 // Use Cases - Restaurant
 import { GetRestaurantsUseCase } from './application/use-cases/restaurant/get-restaurants.use-case';
@@ -21,8 +23,16 @@ import { CreateRestaurantUseCase } from './application/use-cases/restaurant/crea
 import { UpdateRestaurantUseCase } from './application/use-cases/restaurant/update-restaurant.use-case';
 import { DeleteRestaurantUseCase } from './application/use-cases/restaurant/delete-restaurant.use-case';
 
+// Use Cases - Table
+import { GetTablesUseCase } from './application/use-cases/table/get-tables.use-case';
+import { GetTableUseCase } from './application/use-cases/table/get-table.use-case';
+import { CreateTableUseCase } from './application/use-cases/table/create-table.use-case';
+import { UpdateTableUseCase } from './application/use-cases/table/update-table.use-case';
+import { DeleteTableUseCase } from './application/use-cases/table/delete-table.use-case';
+
 // Controllers
 import { AdminRestaurantController } from './infrastructure/http/controllers/admin-restaurant.controller';
+import { AdminTableController } from './infrastructure/http/controllers/admin-table.controller';
 
 @Module({
   imports: [
@@ -34,12 +44,16 @@ import { AdminRestaurantController } from './infrastructure/http/controllers/adm
       GuestOrmEntity,
     ]),
   ],
-  controllers: [AdminRestaurantController],
+  controllers: [AdminRestaurantController, AdminTableController],
   providers: [
     // Repositories
     {
       provide: ADMIN_RESTAURANT_REPOSITORY,
       useClass: AdminRestaurantRepository,
+    },
+    {
+      provide: ADMIN_TABLE_REPOSITORY,
+      useClass: AdminTableRepository,
     },
     // Use Cases - Restaurant
     GetRestaurantsUseCase,
@@ -47,6 +61,12 @@ import { AdminRestaurantController } from './infrastructure/http/controllers/adm
     CreateRestaurantUseCase,
     UpdateRestaurantUseCase,
     DeleteRestaurantUseCase,
+    // Use Cases - Table
+    GetTablesUseCase,
+    GetTableUseCase,
+    CreateTableUseCase,
+    UpdateTableUseCase,
+    DeleteTableUseCase,
   ],
 })
 export class AdminModule {}
