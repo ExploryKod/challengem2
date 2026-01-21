100091d6d2ad9130f81863a98cd2c40eac42b50c
Nasssdev <aissaoui_nassim@outlook.fr>
2026-01-21 21:22:20 +0100

    feat(admin): add in-memory admin reservation repository stub

--- back/src/modules/admin/application/testing/stubs/index.ts
+++ back/src/modules/admin/application/testing/stubs/index.ts
@@ -1,3 +1,4 @@
 export { InMemoryAdminRestaurantRepository } from './in-memory-admin-restaurant.repository';
 export { InMemoryAdminTableRepository } from './in-memory-admin-table.repository';
 export { InMemoryAdminMealRepository } from './in-memory-admin-meal.repository';
+export { InMemoryAdminReservationRepository } from './in-memory-admin-reservation.repository';
