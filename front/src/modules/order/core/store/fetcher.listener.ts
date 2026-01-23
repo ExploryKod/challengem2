bae539ffcf0879596afff19b2d09e3e9900b8916
Nasssdev <aissaoui_nassim@outlook.fr>
2026-01-23 10:37:10 +0100

    feat(order): fetch menus when entering MEALS_PREVIEW step

--- front/src/modules/order/core/store/fetcher.listener.ts
+++ front/src/modules/order/core/store/fetcher.listener.ts
@@ -4,6 +4,7 @@ import { orderingSlice } from "./ordering.slice";
 import { OrderingDomainModel } from "../model/ordering.domain-model";
 import { fetchTables } from "@taotask/modules/order/core/useCase/fetch-table.usecase";
 import { fetchMeals } from "@taotask/modules/order/core/useCase/fetch-meals.usecase";
+import { fetchMenus } from "@taotask/modules/order/core/useCase/fetch-menus.usecase";
 // Event driven --> ici j'ai un évènement sur un évènement
 // Dés que je sélectionne un guest j'arrive sur l'étape suivante (ordering.step.listener.ts s'en occuppe)
 // Ici je veux que dés que j'arrive sur l'étape table je veux que l'on fetch les tables (voir Cours Gateway 20:15)
@@ -13,6 +14,11 @@ export const registerFetcherListeners = (listener: ListenerMiddlewareInstance) =
         actionCreator: orderingSlice.actions.setStep,
             effect: (action, api) => {
             switch(action.payload) {
+                case OrderingDomainModel.OrderingStep.MEALS_PREVIEW: {
+                    api.dispatch(fetchMenus as any);
+                    api.dispatch(fetchMeals as any);
+                    break;
+                }
                 case OrderingDomainModel.OrderingStep.TABLE: {
                     api.dispatch(fetchTables as any);
                     break;
