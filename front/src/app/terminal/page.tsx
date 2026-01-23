f1712c3bd2427812520d18f3bcfa050f6516b1aa
Nasssdev <aissaoui_nassim@outlook.fr>
2026-01-23 04:03:47 +0100

    feat(terminal): create /terminal route

---
+++ front/src/app/terminal/page.tsx
@@ -0,0 +1,20 @@
+import { Metadata } from "next";
+import { Suspense } from "react";
+import { TerminalPage } from "@taotask/modules/terminal/react/pages/TerminalPage";
+
+export const metadata: Metadata = {
+    title: "Terminal - Smart Cafe",
+    description: "Borne de commande",
+    robots: {
+        index: false,
+        follow: false,
+    },
+};
+
+export default function Terminal() {
+    return (
+        <Suspense fallback={<div>Chargement...</div>}>
+            <TerminalPage />
+        </Suspense>
+    );
+}
